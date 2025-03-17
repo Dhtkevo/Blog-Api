152ea595e59371b7bfb7740a217b6d0cbc0fe789
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-03-17 17:51:04 -0400

    Implement queries for comment entity

--- db/queries.ts
+++ db/queries.ts
@@ -46,5 +46,45 @@ export const deletePost = async (postid: number) => {
     where: { id: postid },
   });
 };
+
 // Comment Queries
+export const getAllComments = async () => {
+  const allComments = await prisma.comment.findMany();
+  return allComments;
+};
+
+export const getSpecificComment = async (commentid: number) => {
+  const comment = await prisma.comment.findUnique({ where: { id: commentid } });
+  return comment;
+};
+
+export const createComment = async (content: string, postid: number) => {
+  await prisma.comment.create({
+    data: {
+      content: content,
+      postId: postid,
+    },
+  });
+};
+
+export const updateComment = async (
+  commentid: number,
+  content: string,
+  postid: number
+) => {
+  await prisma.comment.update({
+    where: {
+      id: commentid,
+    },
+    data: {
+      content: content,
+      postId: postid,
+    },
+  });
+};
+
+export const deleteComment = async (commentid: number) => {
+  await prisma.comment.delete({ where: { id: commentid } });
+};
+
 // User Queries
