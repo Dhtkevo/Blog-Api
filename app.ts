f59cb75157305bc865ef85e066e6364e186b8c2a
dhtkevo <110263079+Dhtkevo@users.noreply.github.com>
2025-03-24 11:38:11 -0400

    Enable backend to parse json requests

--- app.ts
+++ app.ts
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "HELLO" });
